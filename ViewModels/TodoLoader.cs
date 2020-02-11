cb0d8c9ce4e3106bf38f9b3b9b477b8c7ab28534
sefe <sebastian.ferngren@gmail.com>
2020-02-11 18:32:48 +0100

    refactor, remove commented out code

--- ViewModels/TodoLoader.cs
+++ ViewModels/TodoLoader.cs
@@ -81,19 +81,6 @@ namespace ViewModels
                 }
             }
 
-            //yield return new TodoItem("First");
-            //await Task.Delay(200);
-            //yield return new TodoItem("Second");
-            //await Task.Delay(200);
-            //yield return new TodoItem("Third");
-            //yield return new TodoItem("a");
-            //yield return new TodoItem("b");
-            //yield return new TodoItem("c half second to next...");
-            //await Task.Delay(500);
-            //yield return new TodoItem("Fource Long wait 2 sec to last ");
-            //await Task.Delay(2000);
-            //yield return new TodoItem("Last one");
-
         }
     }
 }
