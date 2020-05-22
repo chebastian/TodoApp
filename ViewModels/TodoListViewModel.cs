304ccea9e605e51f371a825dea0bf488aeb103d8
sefe <sebastian.ferngren@gmail.com>
2020-05-22 13:33:25 +0200

    refactor, separate TodoPageViewmodel class to its own file

--- ViewModels/TodoListViewModel.cs
+++ ViewModels/TodoListViewModel.cs
@@ -7,49 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Todo;
-using ViewModels.TodoMenu;
 
 namespace ViewModels
 {
-
-    public class TodoPageViewModel : ViewModelBase, TodoListsMenuViewModel.ITodoListSelector
-    {
-        private TodoListViewModel _listViewModel;
-
-        public TodoPageViewModel()
-        {
-            ListViewModel = new TodoListViewModel();
-            MenuViewModel = new TodoListsMenuViewModel(this)
-            {
-                Lists = new ObservableCollection<TodoMenu.TodoListViewModel>()
-                {
-                    new TodoMenu.TodoListViewModel(){Name="saved.txt"},
-                    new TodoMenu.TodoListViewModel(){Name="another.txt"},
-                }
-            };
-        }
-
-        public TodoListViewModel ListViewModel
-        {
-            get
-            {
-                return _listViewModel;
-            }
-
-            set
-            {
-                _listViewModel = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public TodoListsMenuViewModel MenuViewModel { get; set; }
-
-        public void OnListSelected(string name)
-        {
-            ListViewModel.SwitchList(name);
-        }
-    }
 
     public class TodoListViewModel : ViewModelBase
     {
---
+++ ViewModels/TodoPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Sefe.Utils.MVVM;
+using System.Collections.ObjectModel;
+using ViewModels.TodoMenu;
+
+namespace ViewModels
+{
+    public class TodoPageViewModel : ViewModelBase, TodoListsMenuViewModel.ITodoListSelector
+    {
+        private TodoListViewModel _listViewModel;
+
+        public TodoPageViewModel()
+        {
+            ListViewModel = new TodoListViewModel();
+            MenuViewModel = new TodoListsMenuViewModel(this)
+            {
+                Lists = new ObservableCollection<TodoMenu.TodoListViewModel>()
+                {
+                    new TodoMenu.TodoListViewModel(){Name="saved.txt"},
+                    new TodoMenu.TodoListViewModel(){Name="another.txt"},
+                }
+            };
+        }
+
+        public TodoListViewModel ListViewModel
+        {
+            get
+            {
+                return _listViewModel;
+            }
+
+            set
+            {
+                _listViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TodoListsMenuViewModel MenuViewModel { get; set; }
+
+        public void OnListSelected(string name)
+        {
+            ListViewModel.SwitchList(name);
+        }
+    }
+}
