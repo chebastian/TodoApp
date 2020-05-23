10fd0d5e7fdbe840e86e3599da089baa9faedf48
sebastian <sebastian.ferngren@gmail.com>
2020-05-23 18:31:48 +0200

    refactor, move TodoItem to shared Models project

---
+++ TodoModels/TodoItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Todo
+{
+    public class TodoItem
+    {
+        public TodoItem(string name)
+        {
+            Name = name;
+            Completed = false;
+        }
+
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TodoItem item &&
+                   Name == item.Name &&
+                   Completed == item.Completed;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Completed);
+        }
+    }
+}
--- ViewModels/TodoList.cs
+++ ViewModels/TodoList.cs
@@ -1,32 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Todo
 {
-    public class TodoItem
-    {
-        public TodoItem(string name)
-        {
-            Name = name;
-            Completed = false;
-        }
-
-        public string Name { get; set; }
-        public bool Completed { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            return obj is TodoItem item &&
-                   Name == item.Name &&
-                   Completed == item.Completed;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Name, Completed);
-        }
-    }
 
     public class TodoList
     {
